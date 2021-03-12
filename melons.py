22092aab3ab98904c62e5b8fc323e0d3b76cb00f
K. Ravin <77836221+r-a-v-i-n@users.noreply.github.com>
2021-03-12 16:07:07 -0500

    update melons.py Part 3

--- melons.py
+++ melons.py
@@ -1,4 +1,8 @@
 """Classes for melon orders."""
+
+import random
+# import datetime
+
 class AbstractMelonOrder():
     """An abstract base class that other Melon Orders inherit from."""
     def __init__(self, species, qty, order_type, tax):
@@ -8,10 +12,18 @@ class AbstractMelonOrder():
         self.qty = qty
         self.order_type = order_type
         self.tax = tax
+        
+    def get_base_price(self):
+        """Choooses a random integer between 5-9 as base price."""
+        base_price = random.randrange(5, 10)
+
+        base_price = float(base_price)
+
+        return base_price
 
     def get_total(self):
         """Calculate price, including tax."""
-        base_price = 5
+        base_price = self.get_base_price()
 
         if self.species == "Christmas melon":
             base_price = base_price * 1.5
@@ -30,6 +42,7 @@ class AbstractMelonOrder():
 
         self.shipped = True
 
+    
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
@@ -53,9 +66,10 @@ class InternationalMelonOrder(AbstractMelonOrder):
 class GovernmentMelonOrder(AbstractMelonOrder):
     """No tax on government orders"""
     def __init__(self, species, qty):
-        super().__init__(species, qty, "domestic", 0)
+        super().__init__(species, qty, "government", 0)
         self.passed_inspection = False
 
     def mark_inspection(self, passed):
         self.passed_inspection = passed
+        print (self.passed_inspection)
 
