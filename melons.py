18486819c965c86c652b850339435896dcffe847
vagrant <erde0011@umn.edu>
2021-03-12 17:06:22 +0000

    melon orders

---
+++ melons.py
@@ -0,0 +1,51 @@
+"""Classes for melon orders."""
+class AbstractMelonOrder():
+    """An abstract base class that other Melon Orders inherit from."""
+    def __init__(self, species, qty, order_type, tax):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.order_type = order_type
+        self.tax = tax
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        base_price = 5
+
+        if self.species == "Christmas melon":
+            base_price = base_price * 1.5
+
+        if self.qty < 10 and self.order_type == "international":
+
+            total = (1 + self.tax) * self.qty * base_price
+            total += 3
+            
+        total = (1 + self.tax) * self.qty * base_price
+       
+        return total
+        
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty, "domestic", 0.08)
+        """Initialize melon order attributes."""
+        
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty,  "international", 0.17)
+        self.country_code = country_code
+        """Initialize melon order attributes."""
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
